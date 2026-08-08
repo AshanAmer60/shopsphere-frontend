4dc1d7684739c1ae2faf5aec5cab45bb3f5bff92
Ashan Amir <ashanamir27@gmail.com>
2026-08-08 15:43:57 +0500

    proxy removed

--- proxy.tsx
+++ proxy.tsx
@@ -1,27 +1,27 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export function proxy(request: NextRequest) {
-    const { pathname } = request.nextUrl;
-    const accessToken = request.cookies.get("accessToken")?.value;
+// export function proxy(request: NextRequest) {
+//     const { pathname } = request.nextUrl;
+//     const accessToken = request.cookies.get("accessToken")?.value;
 
-    const isProtected = pathname.startsWith("/dashboard");
-    const isAuthPage = 
-    pathname.startsWith("/signin") || pathname.startsWith("/signup");
+//     const isProtected = pathname.startsWith("/dashboard");
+//     const isAuthPage = 
+//     pathname.startsWith("/signin") || pathname.startsWith("/signup");
 
-    if (isProtected && !accessToken) {
-        return NextResponse.redirect(new URL("/signin", request.url));
-    }
+//     if (isProtected && !accessToken) {
+//         return NextResponse.redirect(new URL("/signin", request.url));
+//     }
 
-    if (isAuthPage && accessToken) {
-        return NextResponse.redirect(new URL("/dashboard", request.url));
-    }
+//     if (isAuthPage && accessToken) {
+//         return NextResponse.redirect(new URL("/dashboard", request.url));
+//     }
 
-    return NextResponse.next();
+//     return NextResponse.next();
 
-}
+// }
 
-export const config = {
-    matcher: [
-        "/dashboard/:path*",
-    ],
-}
+// export const config = {
+//     matcher: [
+//         "/dashboard/:path*",
+//     ],
+// }
